a13dc154b4e0eb005eb48513e20c9e9da414e873
marcocotto <124316167+marcocotto@users.noreply.github.com>
2023-08-13 14:11:11 +1200

    Update badapple_easteregg.py

--- Project Hydrate V3 (Final)/badapple_easteregg.py
+++ Project Hydrate V3 (Final)/badapple_easteregg.py
@@ -2,22 +2,26 @@
 Easter egg video page part of Project: Hydrate.
 
 Description:
- - Completely and utterly just an easter egg, made for fun. All this does is just play a video if the user logs in with the username badapple and the password easteregg
- - This part serves no real purpose on the program but is just completely made for fun.
+ - Completely and utterly 
+   just an easter egg, made
+   for fun. All this does is
+   just play a video if the user
+   logs in with the username badapple 
+   and the password easteregg
+ - This part serves no real purpose on the 
+   program but is just completely made for fun.
 """
-
 import subprocess
-from tkinter import *
-
-from tkinter import ttk
 import tkinter as tk
+
+from tkinter import ttk, Tk
 
 from pytube import YouTube
 import vlc
 
-video_url = "https://www.youtube.com/watch?v=UkgK8eUdpAo"
+VIDEO_URL = "https://www.youtube.com/watch?v=UkgK8eUdpAo"
 
-youtube = YouTube(video_url)
+youtube = YouTube(VIDEO_URL)
 root = Tk()
 
 root.title("Project Hydrate: Bad Apple (Easter Egg)")
@@ -36,31 +40,29 @@ def center_window(window):
     height = window.winfo_height()
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
-    x = (screen_width - width) // 2
-    y = (screen_height - height) // 2
-    
-    window.geometry(f"{width}x{height}+{x}+{y}")
-    
+    width_x = (screen_width - width) // 2
+    width_y = (screen_height - height) // 2
+    window.geometry(f"{width}x{height}+{width_x}+{width_y}")
+
 center_window(root)
-root.iconbitmap(default="assets\icon.ico")
+root.iconbitmap(default="assets\\icon.ico")
 
-video_file_path = "assets\\badapple.mp4"
+VIDE_FILE_PATH = "assets\\badapple.mp4"
 
-canvas = tk.Canvas(root, width=640, height=480)
-canvas.pack()
+CANVAS = tk.Canvas(root, width=640, height=480)
+CANVAS.pack()
 
 vlc_instance = vlc.Instance()
 player = vlc_instance.media_player_new()
-media = vlc_instance.media_new(video_file_path)
+media = vlc_instance.media_new(VIDE_FILE_PATH)
 player.set_media(media)
-player.set_hwnd(canvas.winfo_id())
+player.set_hwnd(CANVAS.winfo_id())
 player.play()
 
 def stop_video():
     """
     Stop the video playback and close the window.
     """
-    
     player.stop()
     root.destroy()
     subprocess.call(["python", "main.py"])
