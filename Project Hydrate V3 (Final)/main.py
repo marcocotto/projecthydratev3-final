fcd8dd35335be3d76f6472a65b17d8f995790280
marcocotto <124316167+marcocotto@users.noreply.github.com>
2023-08-13 14:41:50 +1200

    Update main.py

--- Project Hydrate V3 (Final)/main.py
+++ Project Hydrate V3 (Final)/main.py
@@ -2,21 +2,22 @@
 Main page part of Project: Hydrate (first page that will open).
 
 Description:
- - This part of the program is the first thing the user will see when opening the app, it is the part that will give
+ - This part of the program is the first thing the user 
+   will see when opening the app, it is the part that will give
    the user the option to either sign up or log in.
 """
 
 # Imports.
 import subprocess
-from tkinter import *
+import tkinter as tk
 
-from tkinter import ttk
-from PIL import Image, ImageTk, ImageFont
+from tkinter import ttk, Tk
+from PIL import Image, ImageTk
 
 # Create the main window.
 root = Tk()
 root.geometry("495x595")
-root.iconbitmap(default="assets\icon.ico")  # Set the window icon.
+root.iconbitmap(default="assets\\icon.ico")  # Set the window icon.
 
 root.resizable(width=False, height=False)
 root.title("Project Hydrate")  # Set the window title.
@@ -35,12 +36,10 @@ def center_window(window):
     height = window.winfo_height()
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
-    x = (screen_width - width) // 2
-    y = (screen_height - height) // 2
-    
-    window.geometry(f"{width}x{height}+{x}+{y}")
+    width_x = (screen_width - width) // 2
+    width_y = (screen_height - height) // 2
+    window.geometry(f"{width}x{height}+{width_x}+{width_y}")
 
-# Center the main window on the screen
 center_window(root)
 
 # Function to navigate to the login window.
@@ -57,20 +56,20 @@ def goto_signup_window():
     """
     Close the current window and navigate to the signup window.
     """
-    
+
     root.destroy()
     subprocess.call(["python", "signup.py"])
 
 # Load and resize images for the GUI elements.
-menu_icon = Image.open("assets\iconmain.png")
-waves_bottom = Image.open("assets\wavesfullcropped.png")
+menu_icon = Image.open("assets\\iconmain.png")
+waves_bottom = Image.open("assets\\wavesfullcropped.png")
 title_text_icon = Image.open("assets\\titletext.png")
 button_image = Image.open("assets\\buttonimage.png")
 
 menu_icon = menu_icon.resize((108, 141))
 waves_bottom = waves_bottom.resize((495, 395))
 title_text_icon = title_text_icon.resize((182, 70))
-button_image = button_image.resize((200, 30), resample=Image.BICUBIC)
+button_image = button_image.resize((200, 30))
 
 # Convert images to Tkinter-compatible format.
 menu_icon_tk = ImageTk.PhotoImage(menu_icon)
@@ -79,12 +78,32 @@ title_text_icon_tk = ImageTk.PhotoImage(title_text_icon)
 button_image_tk = ImageTk.PhotoImage(button_image)
 
 # Create labels and buttons with the corresponding images.
-menu_icon_label = Label(root, image=menu_icon_tk)
-waves_bottom_label = Label(root, image=waves_bottom_tk)
-title_text_label = Label(root, image=title_text_icon_tk)
-login_button = ttk.Button(root, text="L O G  I N", image=button_image_tk, compound="center", command=goto_login_window)
-signup_button = ttk.Button(root, text="S I G N  U P", image=button_image_tk, compound="center", command=goto_signup_window)
-signin_options_label = Label(root, text="S I G N  I N  O P T I O N S", fg="darkgray", font=("Arial", 14))
+menu_icon_label = tk.Label(root, image=menu_icon_tk)
+waves_bottom_label = tk.Label(root, image=waves_bottom_tk)
+title_text_label = tk.Label(root, image=title_text_icon_tk)
+
+login_button = ttk.Button(
+    root,
+    text="L O G  I N",
+    image=button_image_tk,
+    compound="center",
+    command=goto_login_window
+)
+
+signup_button = ttk.Button(
+    root,
+    text="S I G N  U P",
+    image=button_image_tk,
+    compound="center",
+    command=goto_signup_window
+)
+
+signin_options_label = tk.Label(
+    root,
+    text="S I G N  I N  O P T I O N S",
+    fg="darkgray",
+    font=("Arial", 14)
+)
 
 # Configure button styles.
 style = ttk.Style()
